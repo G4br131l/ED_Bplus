f5febe02554935316d0b172cb4079bc0f58cd41a
Gabriel <gabriel25ribeiro@gmail.com>
2024-09-26 10:29:17 -0300

    alloc e free

--- B+/Bplus.c
+++ B+/Bplus.c
@@ -3,3 +3,38 @@
 #include <stdbool.h>
 #include "Bplus.h"
 
+Bplus *Bplus_alloc(Type type) {
+    Bplus *tree = malloc(sizeof(Bplus));
+    if (!tree) {
+        exit(1);
+    }
+    
+    tree->type = type;
+    tree->root = NULL;
+
+    return tree;
+}
+
+void free_node(node_bplus *node) {
+    free_node(node->left);
+    free_node(node->rigth);
+    free(node);
+}
+
+void Bplus_free(Bplus *tree) {
+    if (!tree) {
+        exit(1);
+    }
+    
+    free_node(tree->root);
+    free(tree);
+}
+
+void Bplus_insert(Bplus *tree, char *value, Type type) {
+    if (!tree || !value || !type || type != tree->type)
+    {
+        exit(1);
+    }
+    
+
+}
--- B+/Bplus.h
+++ B+/Bplus.h
@@ -1,4 +1,4 @@
-typedef enum { INT , FLOAT , STRING } type;
+typedef enum { INT , FLOAT , STRING } Type;
 
 #define ORDER 8
 
@@ -7,13 +7,17 @@ typedef enum { INT , FLOAT , STRING } type;
     int length;
 } Dados; */
 
+typedef struct data {
+    Type type;
+    char value[16];
+} data;
+
 typedef struct node_bplus
 {
     bool folha;
     struct node_bplus *rigth;
     struct node_bplus *left;
-    char *value[ORDER];
-    type type;
+    data values[ORDER];
 } node_bplus;
 
 /* typedef struct {
@@ -25,16 +29,17 @@ typedef struct node_bplus
 
 
 typedef struct Bplus {
-    int id;
+//    int id;
     node_bplus *root;
+    Type type;
 //    char local[16];
 } Bplus;
 
-Bplus *Bplus_alloc();
-Bplus Bplus_free(Bplus *tree);
-void Bplus_insert(Bplus *tree, void *value);
+Bplus *Bplus_alloc(Type);
+void Bplus_free(Bplus *tree);
+void Bplus_insert(Bplus *tree, char *value);
 void Bplus_find(/*  */);
 void Bplus_findInterval(/*  */);
 void Bplus_remove(Bplus *tree, void *value);
 
-void *Bplus_search(Bplus *tree, void *value);
+//void *Bplus_search(Bplus *tree, void *value);
